527e72f3de09ed8ba8ba62dea22e0c6a044e3549
mlop <2923927+marlonlop@users.noreply.github.com>
2023-11-01 19:36:37 -0600

    Update loadDate.scala
    formatDate function

--- loadDate.scala
+++ loadDate.scala
@@ -5,41 +5,42 @@ import java.util.{Calendar, Date}
   * Calculates the load timestamp based on the provided incremental timestamp or the current timestamp as default parameter.
   *
   * @param incremental_ts The input incremental timestamp in the format "yyyy-MM-dd HH:mm:ss."
-  * @param currentTs The current timestamp (optional) in the format "yyyy-MM-dd HH:mm:ss."
   *                 If not provided, the current timestamp will be used.
   * @return The load date in format "yyyy-MM-dd."
   */
 
-def getLoadTs(incremental_ts: String, currentTs: String = ""): String = {
+def getLoadTs(incremental_ts: String = ""): String = {
   val inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val outputFormat = new SimpleDateFormat("yyyy-MM-dd")
 
-  val parsedDate: Date = if (currentTs.isEmpty) {
-    // If currentTs is not provided, use the current timestamp
-    new Date()
+  val parsedDate: Date = if (incremental_ts.isEmpty) {
+    new Date() // Use the current timestamp when incremental_ts is not provided
   } else {
-    // Parse the provided incremental_ts
     inputFormat.parse(incremental_ts)
   }
 
   val calendar: Calendar = Calendar.getInstance()
   calendar.setTime(parsedDate)
-
-  // Subtract one day
-  calendar.add(Calendar.DAY_OF_MONTH, -1)
-
-  // Get the resulting date
+  calendar.add(Calendar.DAY_OF_MONTH, -2)
   val subtractedDate: Date = calendar.getTime()
-
-  // Format the subtracted date to "yyyy-MM-dd" format
-  val formattedDate = outputFormat.format(subtractedDate)
-
-  formattedDate
+  outputFormat.format(subtractedDate)
 }
 
 /*
-//e.g.
-val incremental_ts = "2023-10-31 12:00:00"
-val defaultLoadTs = getLoadTs(incremental_ts) // Uses current timestamp as default
-val customLoadTs = getLoadTs(incremental_ts, "2023-10-25 12:00:00") // Provides custom timestamp
+val defaultLoadTs = getLoadTs() // Uses current timestamp as default
+val customLoadTs = getLoadTs("2023-10-31 12:00:00") // Provides custom incremental timestamp
 */
+
+
+//---------------------------------------------------------------------------------------------------
+import java.text.SimpleDateFormat
+
+def formatDate(incremental_ts: String): String = {
+
+    import java.text.SimpleDateFormat
+    val inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val outputFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val formattedDate = outputFormat.format(inputFormat.parse(incremental_ts))
+
+    return formattedDate
+  }
